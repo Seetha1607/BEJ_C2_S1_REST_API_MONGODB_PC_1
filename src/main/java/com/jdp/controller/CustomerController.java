c768352404a354d34c6cd000812d0b265140e063
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-24 18:59:20 +0800

    written the logic to operate the CRUD methods

--- src/main/java/com/jdp/controller/CustomerController.java
+++ src/main/java/com/jdp/controller/CustomerController.java
@@ -7,18 +7,35 @@
 
 package com.jdp.controller;
 
-import com.jdp.service.CustomerServiceImpl;
+import com.jdp.domain.Customer;
+import com.jdp.service.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 public class CustomerController {
-    private CustomerServiceImpl customerService;
+    private ICustomerService iCustomerService;
 
     @Autowired
-    public CustomerController(CustomerServiceImpl customerService) {
-        this.customerService = customerService;
+    public CustomerController(ICustomerService iCustomerService) {
+        this.iCustomerService = iCustomerService;
+    }
+
+    @PostMapping("/saveCustomerDetails")
+    public ResponseEntity<?> saveCustomerDetails(@RequestBody Customer customer) {
+        return new ResponseEntity<>(iCustomerService.saveCustomerDetails(customer), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fetchCustomerDetails")
+    public ResponseEntity<?> fetchCustomerDetails() {
+        return new ResponseEntity<>(iCustomerService.fetchAllCustomerDetails(), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/deleteCustomer/{customerId}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable int customerId) {
+        return new ResponseEntity<>(iCustomerService.deleteCustomerDetail(customerId), HttpStatus.OK);
     }
 }
