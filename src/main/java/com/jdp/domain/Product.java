1ad49c4798d0f3c21e6f95921b81843949709bfe
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-24 18:43:24 +0800

    added the no args and parametrize constructor,toString() methods, getter() and setter() methods in Product class

--- src/main/java/com/jdp/domain/Product.java
+++ src/main/java/com/jdp/domain/Product.java
@@ -7,8 +7,52 @@
 
 package com.jdp.domain;
 
+import java.util.StringJoiner;
+
 public class Product {
     private int productId;
     private String productName;
     private String productDescription;
+
+    public Product() {
+    }
+
+    public Product(int productId, String productName, String productDescription) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productDescription = productDescription;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Product.class.getSimpleName() + "[", "]")
+                .add("productId=" + productId)
+                .add("productName='" + productName + "'")
+                .add("productDescription='" + productDescription + "'")
+                .toString();
+    }
 }
