c0d483958c9f0d5213a5f53a40962601721c4f9e
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-24 18:51:26 +0800

    implemented the abstract methods from ICustomerService and autowired the CustomerRepository to handle the functions

--- src/main/java/com/jdp/service/CustomerServiceImpl.java
+++ src/main/java/com/jdp/service/CustomerServiceImpl.java
@@ -7,5 +7,35 @@
 
 package com.jdp.service;
 
-public class CustomerServiceImpl {
+import com.jdp.domain.Customer;
+import com.jdp.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements ICustomerService {
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public Customer saveCustomerDetails(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> fetchAllCustomerDetails() {
+        return null;
+    }
+
+    @Override
+    public boolean deleteCustomerDetail(int customerId) {
+        return false;
+    }
 }
