1e41392f78e80bb6f67287e44a6ac4e1eb83f893
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-24 18:41:15 +0800

    added no args and parameterized constructor , toString() and getter() and setter() methods in Customer class

--- src/main/java/com/jdp/domain/Customer.java
+++ src/main/java/com/jdp/domain/Customer.java
@@ -10,6 +10,8 @@ package com.jdp.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.StringJoiner;
+
 @Document
 public class Customer {
     @Id
@@ -18,5 +20,55 @@ public class Customer {
     private String customerPhoneNumber;
     private Product customerProduct;
 
+    public Customer() {
+    }
 
+    public Customer(int customerId, String customerName, String customerPhoneNumber, Product customerProduct) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerPhoneNumber = customerPhoneNumber;
+        this.customerProduct = customerProduct;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public Product getCustomerProduct() {
+        return customerProduct;
+    }
+
+    public void setCustomerProduct(Product customerProduct) {
+        this.customerProduct = customerProduct;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Customer.class.getSimpleName() + "[", "]")
+                .add("customerId=" + customerId)
+                .add("customerName='" + customerName + "'")
+                .add("customerPhoneNumber='" + customerPhoneNumber + "'")
+                .add("customerProduct=" + customerProduct)
+                .toString();
+    }
 }
